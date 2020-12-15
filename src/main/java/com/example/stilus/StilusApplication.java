0bec48d3d98fc668b5425c580c5c4a725fbc97e2
makifoz <ozdemir.mhmtakif@gmail.com>
2020-12-15 19:23:46 +0300

    init

---
+++ src/main/java/com/example/stilus/StilusApplication.java
@@ -0,0 +1,13 @@
+package com.example.stilus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StilusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StilusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stilus/StilusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stilus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StilusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
